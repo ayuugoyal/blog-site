10b6cfc0bef1e0d701d655f62e41a2e555cdbb8b
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-04-03 22:11:04 +0530

    starting a blog site

---
+++ src/components/navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Blogs from "../pages/blogs";
+import ContactUs from "../pages/contactUs";
+import Home from "../pages/home";
+
+const Navbar = () => {
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <ul className="flex justify-center space-x-10 text-2xl p-4">
+            <li className="">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/blogs" element={<Blogs />}></Route>
+          <Route exact path="/contact" element={<ContactUs />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>this is the main blog site</div>
+      <div>cards</div>
+    </>
+  );
+};
+
+export default Home;
